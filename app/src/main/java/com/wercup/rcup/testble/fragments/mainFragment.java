d5a7dc2d8d63d894896a347394f6e30954e77a68
KeiiLo <sengdao.le@gmail.com>
2016-11-18 15:10:02 +0100

    Can send a read config instruction

--- app/src/main/java/com/wercup/rcup/testble/fragments/mainFragment.java
+++ app/src/main/java/com/wercup/rcup/testble/fragments/mainFragment.java
@@ -1,109 +1,136 @@
 package com.wercup.rcup.testble.fragments;
 
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCharacteristic;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.wercup.rcup.testble.BLEService.BLEService;
+import com.wercup.rcup.testble.MainActivity;
 import com.wercup.rcup.testble.R;
+import com.wercup.rcup.testble.tools.SensorTagData;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link mainFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link mainFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class mainFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    public TextView mXAccel, mYAccel, mZAccel, mTapTap, mTemp, mStep, mBattery, mBattery2, mRefresh;
+    public Button mReadConfig, mSendConfig;
 
-    private OnFragmentInteractionListener mListener;
+    public MainActivity mainActivity;
 
     public mainFragment() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment mainFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static mainFragment newInstance(String param1, String param2) {
+    public static mainFragment newInstance() {
         mainFragment fragment = new mainFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        mainActivity = (MainActivity) getActivity();
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        mXAccel = (TextView) view.findViewById(R.id.text_X);
+        mYAccel = (TextView) view.findViewById(R.id.text_Y);
+        mZAccel = (TextView) view.findViewById(R.id.text_Z);
+        mTapTap = (TextView) view.findViewById(R.id.text_taptap);
+        mTemp = (TextView) view.findViewById(R.id.text_temperature);
+        mStep = (TextView) view.findViewById(R.id.text_step);
+        mBattery = (TextView) view.findViewById(R.id.text_battery);
+        mBattery2 = (TextView) view.findViewById(R.id.text_battery2);
+        mRefresh = (TextView) view.findViewById(R.id.text_refresh_rate);
+        mReadConfig = (Button) view.findViewById(R.id.btn_read);
+        mReadConfig.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*byte[] setConfig = new byte[]{(byte) 0x30, (byte) 0x28, (byte) 0x00};
+                byte[] readConfig = new byte[]{(byte) 0x31};
+                BluetoothGattCharacteristic configChar = mGatt.getService(PRESSURE_SERVICE).getCharacteristic(PRESSURE_CONFIG_CHAR);
+                if (count % 2 == 0) {
+                    configChar.setValue(setConfig);
+                } else {
+                    configChar.setValue(readConfig);
+                }
+                count++;
+                gatt.writeCharacteristic(configChar);*/
+                Log.e("ButtonClick", "Clicking on read");
+                byte[] readConfig = new byte[]{(byte) 0x11};
+                BluetoothGatt gatt = mainActivity.getmBLEService().getmGatt();
+                BluetoothGattCharacteristic readConfigChar = gatt.getService(BLEService.ENERGY_SERVICE).getCharacteristic(BLEService.ENERGY_CONFIG_CHAR);
+                readConfigChar.setValue(readConfig);
+                gatt.writeCharacteristic(readConfigChar);
+            }
+        });
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+
+    /* Methods to extract sensor data and update the UI */
+    private int[] values;
+
+    public void updateAccelValues(BluetoothGattCharacteristic characteristic) {
+        values = SensorTagData.extractAccelCoefficients(characteristic);
+        mXAccel.setText(String.valueOf(values[0]));
+        mYAccel.setText(String.valueOf(values[1]));
+        mZAccel.setText(String.valueOf(values[2]));
+        mTemp.setText(String.valueOf(values[3]));
+    }
+
+    public void updateEnergyValue(BluetoothGattCharacteristic characteristic) {
+        double[] batteryLevel = SensorTagData.getBatteryLevel(characteristic);
+        mBattery.setText(String.valueOf(batteryLevel[0]) + "mA");
+        mBattery2.setText(String.valueOf(batteryLevel[1]) + "V");
+    }
+
+
+    public void updatePressureValue(BluetoothGattCharacteristic characteristic) {
+        if (SensorTagData.isTapTap(characteristic)) {
+            mTapTap.setText("True");
+        } else {
+            mTapTap.setText("False");
         }
+        int steps = SensorTagData.getSteps(characteristic);
+        mStep.setText(String.valueOf(steps));
+    }
+
+
+    public void clearDisplayValues() {
+        mXAccel.setText("---");
+        mYAccel.setText("---");
+        mZAccel.setText("---");
+        mTapTap.setText("---");
+        mTemp.setText("---");
+        mStep.setText("---");
+        mBattery.setText("---");
+        mBattery2.setText("---");
+        mRefresh.setText("---");
     }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
     }
 }
